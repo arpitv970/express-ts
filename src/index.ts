13f5839e5554cc1bef7cfd86dd50abfd0843df8a
Arpit Verma <arpitv970@gmail.com>
2024-06-23 19:14:58 +0530

    Updated for `tsconfig.json` && `package.json` && Project structure
    - Fixed `rootDir` as `./src` && `outDir` as `./dist`
    - Fixed `start` script for `./dist` directory
    - Improved Code structure by using `MVC` architecture

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { PORT } from './config/dotenv';
+import { router as indexRouter } from './routers'
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+
+app.use('/api/v1', indexRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
